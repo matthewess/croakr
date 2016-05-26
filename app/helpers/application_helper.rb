916727f68747f61376a161ed57cfcb07518aa493
bmatt <mess@purdue.edu>
2016-05-26 16:23:37 -0400

    title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #returns full title for a page, or the trimmed base title if necessary
+  def title_for page_title=""
+    base_title = "croakr"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
