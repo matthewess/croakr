e2a1e2bfeb442a4d5f5b3cea6c6f3810b9084210
bmatt <mess@purdue.edu>
2016-05-26 16:23:46 -0400

    testing

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "croakr"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "about | croakr"
+  end
+
+  test "should get signin" do
+    get :signin
+    assert_response :success
+    assert_select "title", "sign-in | croakr"
+  end
+
+  test "should get splash" do
+    get :splash
+    assert_response :success
+    assert_select "title", "welcome | croakr"
+  end
+
 end
