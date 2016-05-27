617243a0a5bff6689b8a918052f5bfe343eb16d3
bmatt <mess@purdue.edu>
2016-05-26 23:04:14 -0400

    emails are lowercased

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A\w+@\w+\.\w+\z/i
+
+  before_save { self.email = self.email.downcase }
   validates :name, presence: true, 
       length: { maximum: 25 }
   validates :email, presence: true, 
