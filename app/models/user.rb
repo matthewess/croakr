275f950f80640a16754b24b273d257b97f56e77d
bmatt <mess@purdue.edu>
2016-05-26 22:57:41 -0400

    added more email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A\w+@\w+\.\w+\z/i
+  validates :name, presence: true, 
+      length: { maximum: 25 }
+  validates :email, presence: true, 
+      length: { maximum: 255 }, 
+      format: { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
 end
