925004fe94e20062e11eacae44ceacb1cb42a16e
bmatt <mess@purdue.edu>
2016-05-26 23:47:57 -0400

    added secure password and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
+
   VALID_EMAIL_REGEX = /\A\w+@\w+\.\w+\z/i
 
   before_save { self.email = self.email.downcase }
+
   validates :name, presence: true, 
       length: { maximum: 25 }
   validates :email, presence: true, 
       length: { maximum: 255 }, 
       format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
 end
